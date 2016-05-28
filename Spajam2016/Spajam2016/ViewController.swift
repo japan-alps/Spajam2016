5b1d85b7137a50d9c62c6a1d89e263e6b6245304
高畑 孝輝 <takahata@takabata-no-MacBook-Air.local>
2016-05-28 17:46:44 +0900

    first commit

---
+++ Spajam2016/Spajam2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spajam2016
+//
+//  Created by 高畑 孝輝 on 2016/05/28.
+//  Copyright © 2016年 japan-alps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
