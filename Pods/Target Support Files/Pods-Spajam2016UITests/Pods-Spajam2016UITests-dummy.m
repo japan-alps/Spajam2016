ae781378a425bdf6e6718241ef7cc70a4da8d763
会津慎弥 <b1013043@fun.ac.jp>
2016-05-28 23:04:31 +0900

    add drawing function

---
+++ Pods/Target Support Files/Pods-Spajam2016UITests/Pods-Spajam2016UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spajam2016UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Spajam2016UITests
+@end
--- Spajam2016/ViewController.swift
+++ Spajam2016/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import ACEDrawingView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var drawingView: ACEDrawingView!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var penButton: UIButton!
+    @IBOutlet weak var eraserButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initDrawingView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +28,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func initDrawingView(){
+        drawingView.lineWidth = 3
+        drawingView.drawTool = ACEDrawingToolTypePen
+    }
+    
+    @IBAction func penAction(sender: UIButton) {
+        drawingView.lineWidth = 3
+        drawingView.drawTool = ACEDrawingToolTypePen
+    }
+    
+    @IBAction func eraserAction(sender: UIButton) {
+         drawingView.lineWidth = 20
+        drawingView.drawTool = ACEDrawingToolTypeEraser
+    }
 
+    // 全消し
+    @IBAction func resetAction(sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Clear", message: "編集したデータを全て削除し、\n白紙に戻しますか?", preferredStyle: .Alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Default) {
+            action in
+            
+            self.drawingView.clear()
+        }
+        let cancellAction = UIAlertAction(title: "Cancell", style: .Cancel, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        alertController.addAction(cancellAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    //画像をNSDataに変換
+    func ImageToString(image:UIImage) -> String? {
+        let data:NSData = UIImagePNGRepresentation(image)!
+        //NSDataへの変換が成功していたら
+        if let pngData:NSData = data {
+            //BASE64のStringに変換する
+            let encodeString:String =
+                pngData.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
+            return encodeString
+        }
+        return nil
+    }
 
 }
 
